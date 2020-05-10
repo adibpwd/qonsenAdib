8154aefd13b283210c885b82377fa4443983f0e1
muslikhuladib0 <54851209+muslikhuladib0@users.noreply.github.com>
2020-05-10 08:29:38 +0700

    Update send.php

--- send.php
+++ send.php
@@ -4,24 +4,26 @@ sendMessage($_POST);
  
 function tg($url, $params)
 {
-    $curl = curl_init();
-    curl_setopt($curl, CURLOPT_URL, $url . $params);
-    curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
-    curl_setopt($curl, CURLOPT_TIMEOUT, 3);
-    $content = curl_exec($curl);
-    curl_close($curl);
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $url . $params); 
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($ch, CURLOPT_TIMEOUT, 3);
+    $content = curl_exec($ch);
+    curl_close($ch);
     return json_decode($content, true);
+
 }
 
 function sendMessage($test) 
-{ 
- $key = 'bot1007203428:AAGcf2X38w1nOuYDMa6U01JACr6C1ETFxaE';
-    $chat = tg('https://api.telegram.org/' . $key . '/getUpdates', '');
-
+{
+    $key = 'bot1037893467:AAG4RVfohWQQRy0AwOuv-s4KyDVyZi4bqKs';
+    // echo 'l';
+    $chat = tg('https://api.telegram.org/' . $key . '/getMe', '');
+    var_dump($chat); die;
     if ($chat['ok']) { 
         $text = 'nama depan : ' . $test['depan'] . ' nama belakang : ' . $test['belakang'];
         $text = urlencode($text);
     }
 
-    return tg('https://api.telegram.org/' . $key . '/sendMessage', '?chat_id=-1001417167352&text=' . $text);
+    return tg('https://api.telegram.org/' . $key . '/sendMessage', '?chat_id=-479075393&text=' . $text);
 }
