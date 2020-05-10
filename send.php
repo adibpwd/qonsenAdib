4cb6bc3388b52c478dfdfb4b3d7612916cd69f27
muslikhuladib0 <54851209+muslikhuladib0@users.noreply.github.com>
2020-05-10 08:33:24 +0700

    Update send.php

--- send.php
+++ send.php
@@ -18,8 +18,8 @@ function sendMessage($test)
 {
     $key = 'bot1037893467:AAG4RVfohWQQRy0AwOuv-s4KyDVyZi4bqKs';
     // echo 'l';
-    $chat = tg('https://api.telegram.org/' . $key . '/getMe', '');
-    var_dump($chat); die;
+    $chat = tg('https://api.telegram.org/' . $key . '/getUpdates', '');
+//     var_dump($chat); die;
     if ($chat['ok']) { 
         $text = 'nama depan : ' . $test['depan'] . ' nama belakang : ' . $test['belakang'];
         $text = urlencode($text);
